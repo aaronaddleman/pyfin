e048b0f6b5d05950d8c74fed527665bf1b53d153
Aaron Addleman <aaronaddleman@gmail.com>
2020-05-09 22:33:19 -0700

    Setting strategies for RedWhiteBlue
    - If applied, this commit will...
    Allows to add a strategy to a stock
    
    - Explain why this change is being made...
    This adds a strategy to a stock. Possible to add multiple strategies to a single
    stock.
    
    - Provide links to any relevant tickets, articles, or other resources...

--- pyfin/pyfin.py
+++ pyfin/pyfin.py
@@ -12,7 +12,8 @@ class Stock():
         self.startdate = startDate
         self.enddate = endDate
         self.exp_moving_averages = [3,5,8,10,12,15,30,35,40,45,50,60]
-        self.strategies = []
+        self.validate_stocks()
+        self.strategies = {}
 
     def getData(self):
         '''Fetch the stock data for the date range provided'''
@@ -27,84 +28,76 @@ class Stock():
         if remove_nil :
             self.stock_data = self.stock_data.iloc[moving_average_days:]
 
+    def validate_stocks(self):
+        try:
+            # find remainder of dividing by 2
+            # and compare if the remainder is 0
+            len(self.exp_moving_averages) % 2 == 0
+        except:
+            print("current exp moving avg: ")
+            print(self.exp_moving_averages)
+            raise Exception('exp_moving_averages should be divisible by 2')
+
     def addExpMovingAverage(self):
         for moving_average in self.exp_moving_averages:
             mean_value = self.stock_data.loc[:,'Adj Close'].ewm(span=moving_average, adjust=False).mean()
             self.stock_data["Ema_"+str(moving_average)] = round(mean_value, 2)
 
 class Strategy():
-    def __init__(self, stock_list):
-        self.stock_list = stock_list
-        self.validate_stocks()
+    def __init__(self, name=None):
+        self.name = name
+        self.results = {'df': None, 'success_rate': 0, 'percentchange': None}
 
-    def validate_stocks(self):
-        for stock in self.stock_list:
-            try:
-                # find remainder of dividing by 2
-                # and compare if the remainder is 0
-                len(stock.exp_moving_averages) % 2 == 0
-            except:
-                print("current exp moving avg: ")
-                print(stock.exp_moving_averages)
-                raise Exception('exp_moving_averages should be divisible by 2')
+    def red_white_blue(self, dataframe=None):
+        # build vars for socks
+        # filter columns based on name starting with Ema_
+        df = dataframe.copy()
+        exp_mov_avg = [col for col in df if col.startswith('Ema_')]
+        exp_mov_avg_half_int = len(exp_mov_avg) / 2
+        exp_mov_avg_half_cnt = 1
+        exp_mov_avg_min = []
+        exp_mov_avg_max = []
 
-    def red_white_blue(self):
-        for stock in self.stock_list:
-            # build vars for socks
-            # filter columns based on name starting with Ema_
-            exp_mov_avg = [col for col in stock.stock_data if col.startswith('Ema_')]
-            exp_mov_avg_half_int = len(exp_mov_avg) / 2
-            exp_mov_avg_half_cnt = 1
-            exp_mov_avg_min = []
-            exp_mov_avg_max = []
+        # build min and max moving averages
+        for exp in exp_mov_avg:
+            if exp_mov_avg_half_cnt <= exp_mov_avg_half_int:
+                # if the half count is less than or equal
+                # to length of exp_mov_avg, it is a min value
+                exp_mov_avg_min.append(exp)
+                exp_mov_avg_half_cnt += 1
+            else:
+                exp_mov_avg_max.append(exp)
 
-            # build min and max moving averages
-            for exp in exp_mov_avg:
-                if exp_mov_avg_half_cnt <= exp_mov_avg_half_int:
-                    # if the half count is less than or equal
-                    # to length of exp_mov_avg, it is a min value
-                    exp_mov_avg_min.append(exp)
-                    exp_mov_avg_half_cnt += 1
-                else:
-                    exp_mov_avg_max.append(exp)
+        df["Exp_Min"] = df[exp_mov_avg_min].min(axis=1)
+        df["Exp_Max"] = df[exp_mov_avg_max].max(axis=1)
+        pos = 0
+        num = 0
+        percentchange = []
 
-            df = stock.stock_data
-            df["Exp_Min"] = stock.stock_data[exp_mov_avg_min].min(axis=1)
-            df["Exp_Max"] = stock.stock_data[exp_mov_avg_max].max(axis=1)
+        # if exp_min is greater than exp_max, buy
+        # if not, then sell
+        for i in df.index:
+            cmin = df['Exp_Min'][i]
+            cmax = df['Exp_Max'][i]
+            close = df['Adj Close'][i]
 
-            pos = 0
-            num = 0
-            percentchange = []
-
-            # if exp_min is greater than exp_max, buy
-            # if not, then sell
-            for i in df.index:
-                cmin = df['Exp_Min'][i]
-                cmax = df['Exp_Max'][i]
-                close = df['Adj Close'][i]
-
-                if (cmin > cmax):
-                    if (pos == 0):
-                        bp = close
-                        pos = 1
-                        print("buying now at " + str(bp))
-
-                elif (cmin < cmax):
-                    if (pos == 1):
-                        pos = 0
-                        sp = close
-                        print("selling now at " + str(sp))
-                        pc = (sp/bp-1)*100
-                        percentchange.append(pc)
-
-                if (num == df["Adj Close"].count()-1 and pos == 1):
+            if (cmin > cmax):
+                if (pos == 0):
+                    bp = close
+                    pos = 1
+            elif (cmin < cmax):
+                if (pos == 1):
                     pos = 0
                     sp = close
-                    print("selling now at " + str(sp))
                     pc = (sp/bp-1)*100
                     percentchange.append(pc)
 
-                num+=1
-
-
-            print(percentchange)
+            if (num == df["Adj Close"].count()-1 and pos == 1):
+                pos = 0
+                sp = close
+                print("selling now at " + str(sp))
+                pc = (sp/bp-1)*100
+                percentchange.append(pc)
+            num+=1
+        self.results['df'] = df
+        self.results['percentchange'] = percentchange
